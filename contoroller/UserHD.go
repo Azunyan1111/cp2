d01029071c46a1bc2dbdc89b621694c98171aa1b
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-11-05 02:25:52 +0900

    add get test user data

--- contoroller/UserHD.go
+++ contoroller/UserHD.go
@@ -3,11 +3,17 @@ package contoroller
 import (
 	"github.com/labstack/echo"
 	"net/http"
+	"github.com/Azunyan1111/cp/model"
+	"strconv"
 )
 
 func GetUserHD() echo.HandlerFunc {
 	return func(c echo.Context) error { //c をいじって Request, Responseを色々する
-		return c.String(http.StatusOK, "You name is "+c.Param("userId"))
+		if c.Param("userId") == strconv.FormatInt(model.TestUser.Id,10){
+			return c.String(http.StatusOK, "You name is " + model.TestUser.Name)
+		}else{
+			return c.String(http.StatusBadRequest, c.Param("userId") + " is not found user.")
+		}
 	}
 }
 
