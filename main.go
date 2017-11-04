4ac50579bc1a1e8b65bd52a889706ab01a9c3745
Azunyan1111 <s15a1302@bene.fit.ac.jp>
2017-11-05 00:39:20 +0900

    add url

--- main.go
+++ main.go
@@ -8,6 +8,12 @@ import (
 
 
 func main() {
-	http.HandleFunc("/", model.HelloWolrdHD) // ハンドラを登録してウェブページを表示させる
+	http.HandleFunc("/", model.HelloWolrdHD) // Hello World と表示するだけのチエック用
+	// ユーザー情報関連
+	http.HandleFunc("/login", model.HelloWolrdHD) // ログイン兼サインアップ（ＰＯＳＴ）
+	http.HandleFunc("/user/:userId", model.HelloWolrdHD) // プロフィール取（ＧＥＴ）・プロフィール更新（ＰＵＴ）
+	// 請求
+	http.HandleFunc("/request", model.HelloWolrdHD) // ポイント請求ＵＲＬ＆ＱＲコード生成（ＰＯＳＴ）
+	http.HandleFunc("/payment/:param", model.HelloWolrdHD) // ポイント支払い
 	http.ListenAndServe(":" + os.Getenv("PORT"), nil)
 }
